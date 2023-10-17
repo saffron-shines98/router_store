8db81d05ef1cfeca81e1a8eeea429d8a30329332
Suraj Kesarwani <surajkesarwani2@gmail.com>
2023-10-17 16:49:12 +0530

    order status update

--- app/retail/v1/order/service.py
+++ app/retail/v1/order/service.py
@@ -1,4 +1,5 @@
 from config import Config
+from datetime import datetime
 from app.retail.v1.order.order_coordinator import OrderCoordinator
 
 class OrderService:
@@ -12,19 +13,22 @@ class OrderService:
         jwt_token = self.headers.get('Jwt')
         nodesso_id = self.headers.get('Nodesso-Id')
         payload = {
-            'token': jwt_token,
-            'nodessoId': nodesso_id,
+            'nodesso_id': nodesso_id,
+            'auth_token': jwt_token
         }
         api_response = self.coordinator.validate_jwt(payload)
-        challenging_string = api_response.get('payload').get('challenge_string')
+        created_time = self.params.get('status_created_time')
+        input_format = "%d:%m:%Y %H:%M:%S"
+        output_format = "%Y:%m:%d %H:%M:%S"
+        parsed_date = datetime.strptime(created_time, input_format)
+        converted_date_time = parsed_date.strftime(output_format)
         order_payload = {
             "order_id": self.params.get('order_id'),
             "order_status": self.params.get('order_status'),
-            "fulfilment_status": self.params.get('fulfillment_status'),
+            "fulfilment_status": self.params.get('fulfilment_status'),
             "refund_status": self.params.get('refund_status'),
-            "status_created_time": self.params.get('status_created_time'),
+            "status_created_time": converted_date_time,
             "remark": self.params.get('remark'),
-            "challenge_string":challenging_string
         }
         self.coordinator.save_data_in_db(order_payload, 'plotch_order_status_request')
         return order_payload
