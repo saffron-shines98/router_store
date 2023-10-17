32baf0be4538851f6e3cceb8224365c428f33231
Suraj Kesarwani <surajkesarwani2@gmail.com>
2023-10-17 11:07:17 +0530

    order status update

--- app/retail/v1/order/order_coordinator.py
+++ app/retail/v1/order/order_coordinator.py
@@ -1,9 +1,5 @@
-import pymongo
 from config import Config
-from pymongo import MongoClient
-from redis import ResponseError
 from app.base_coordinator import BaseCoordinator,SSOCoordinator
-import pymongo
 
 
 class OrderCoordinator(BaseCoordinator):
@@ -11,8 +7,9 @@ class OrderCoordinator(BaseCoordinator):
     def __init__(self):
         super(OrderCoordinator, self).__init__()
         self.sso_coordinator =SSOCoordinator()
+
     def validate_jwt(self, payload):
-        response = self.sso_coordinator.post('/verifyHeader', payload=payload, headers={'Authorization':Config.Authorization})
+        response = self.sso_coordinator.post('/verify-header', payload=payload, headers={'Authorization':Config.Authorization})
         if response.status_code == 200:
             return response.json().get('d')
         raise Exception('Something Went wrong Please try again later')
