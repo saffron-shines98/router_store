072870ab585d7cae61878d42c5dcd80e1d18c349
shashikant vishwakarma <shashikant.vishwakarma@craftsvilla.com>
2023-10-24 13:53:11 +0530

    add skip vallidation code

--- app/retail/v1/product/service.py
+++ app/retail/v1/product/service.py
@@ -18,10 +18,10 @@ class ProductService:
         self.headers.update({"Skip-Validation": self.headers.get('Skip-Validation') or "1",
                              "Storefront-Id": self.params.get('noderetail_storefront_id', ''),
                              "Authorization": Config.API_ACCESS_KEY})
-        db_params = {'payload': json.dumps(self.params), 'headers': json.dumps(self.headers),
-            'status': 0, 'created_at': get_current_datetime(), 'created_by': 1}
-        request_id = self.coordinator.save_data_in_db_with_place_holder(db_params, 'product_create_request_log')
-        self.coordinator.push_data_in_queue({"request_id": request_id}, 'product_create_request_log_q')
+        db_params = {'request': json.dumps(self.params), 'headers': json.dumps(self.headers),
+            'status': 0, 'created_at': get_current_datetime(), 'created_by': 1, 'type': 'product'}
+        entity_id = self.coordinator.save_data_in_db_with_place_holder(db_params, 'plotch_noderetailapi_request_logs')
+        self.coordinator.push_data_in_queue({"entity_id": entity_id}, 'product_create_request_log_q')
         if not self.headers.get('Auth-Token', ''):
             raise AuthMissing('Auth token is missing')
         payload = {'nodesso_id': self.headers.get('Nodesso-Id', ''),'auth_token': self.headers.get('Auth-Token', '')}
