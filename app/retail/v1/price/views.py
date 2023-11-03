2c2ee651cb86b69511873606df76624a1d831d2e
Suraj Kesarwani <surajkesarwani2@gmail.com>
2023-11-03 14:45:50 +0530

    pr for price update

---
+++ app/retail/v1/price/views.py
@@ -0,0 +1,23 @@
+from flask.views import MethodView
+from app.decorators import validate_params
+from app.common_utils import render_success_response
+from app.retail.v1.price.service import PriceService
+class PriceUpdate(MethodView):
+    param_config = {
+        'type': 'object',
+        'properties': {
+            'item_id': {
+                'type': 'string'
+            },
+            'mrp': {
+                'type': 'number'
+            },
+            'storefront_instance_id': {
+                'type': 'number'
+            }
+        }
+    }
+    @validate_params(param_config=param_config)
+    def post(self, params, headers, *args, **kwargs):
+        response = PriceService(params, headers).update_price()
+        return render_success_response(response, msg='success')
