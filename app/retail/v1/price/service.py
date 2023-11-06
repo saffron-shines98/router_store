733946af4bc2c1e828628d88bf99934581c0a91c
Suraj Kesarwani <surajkesarwani2@gmail.com>
2023-11-06 12:53:16 +0530

    pr for price update

--- app/retail/v1/price/service.py
+++ app/retail/v1/price/service.py
@@ -1,6 +1,5 @@
 import json
 from app.common_utils import get_current_datetime
-from app.exceptions import AuthMissing
 from app.retail.v1.price.price_coordinator import PriceCoordinator
 
 class PriceService:
@@ -22,19 +21,10 @@ class PriceService:
         }
         return self.coordinator.save_data_in_db(log_params, 'plotch_noderetailapi_request_logs')
 
-    def authenticate_user(self):
-        jwt_token = self.headers.get('Auth-Token')
-        nodesso_id = self.headers.get('Nodesso-Id')
-        if not jwt_token:
-            raise AuthMissing('Auth token is missing')
-        payload = {
-            'nodesso_id': nodesso_id,
-            'auth_token': jwt_token
-        }
-        self.coordinator.validate_jwt(payload)
-
     def update_price(self):
         entity_id = self.generate_api_logs('price', self.params.get('noderetail_item_id'), self.params.get('noderetail_storefront_id'))
-        self.authenticate_user()
+        jwt_token= self.headers.get('Auth-Token')
+        nodesso_id = self.headers.get('Nodesso-Id')
+        self.coordinator.authenticate_user(jwt_token, nodesso_id)
         self.coordinator.push_data_in_queue({'entity_id':entity_id}, 'noderetail_price_update_sync_q')
         return {}
