97422345e20af9492b9fcc0ef8f081399479d6b3
Suraj Kesarwani <surajkesarwani2@gmail.com>
2023-11-06 12:53:44 +0530

    pr for price update

--- app/retail/v1/price/price_coordinator.py
+++ app/retail/v1/price/price_coordinator.py
@@ -8,9 +8,3 @@ class PriceCoordinator(BaseCoordinator):
     def __init__(self):
         super(PriceCoordinator, self).__init__()
         self.sso_coordinator =SSOCoordinator()
-
-    def validate_jwt(self, payload):
-        response = self.sso_coordinator.post('/verifyHeader', payload=payload, headers={'Authorization':Config.Authorization})
-        if response.status_code == 200:
-            return response.json().get('d')
-        raise InvalidAuth('Invalid auth token.')
