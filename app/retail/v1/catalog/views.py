bf4465c1856fb1b9f1db901b968bf98eb20a3eab
shashikant vishwakarma <shashikant.vishwakarma@craftsvilla.com>
2023-12-05 16:14:23 +0530

    cached

--- app/retail/v1/catalog/views.py
+++ app/retail/v1/catalog/views.py
@@ -13,6 +13,6 @@ class FetchCatalog(MethodView):
     }
     @validate_params(param_config=param_config)
     def post(self, params, headers, *args, **kwargs):
-        response = CatalogService(params, headers).fetch_catalog()
-        return render_success_response_with_body(response, msg='success')
+        response, msg = CatalogService(params, headers).fetch_catalog()
+        return render_success_response_with_body(response, msg=msg)
     
