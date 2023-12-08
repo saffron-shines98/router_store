3c645c334dcb6a99ed368c397e6eb2985efe16d0
shashikant vishwakarma <shashikant.vishwakarma@craftsvilla.com>
2023-12-08 14:05:58 +0530

    add es code and connection

--- app/retail/v1/catalog/views.py
+++ app/retail/v1/catalog/views.py
@@ -16,3 +16,16 @@ class FetchCatalog(MethodView):
         response = CatalogService(params, headers).fetch_catalog()
         return render_success_response_with_body(response, msg='success')
     
+
+class FetchCatalogFromEs(MethodView):
+    param_config = {
+        'type': 'object',
+        'properties': {
+            'noderetail_storefront_id':{'type': 'string'}
+        },
+        'required': ['noderetail_storefront_id']
+    }
+    @validate_params(param_config=param_config)
+    def post(self, params, headers, *args, **kwargs):
+        response = CatalogService(params, headers).fetch_catalog_from_es()
+        return render_success_response_with_body(response, msg='Fetch Catalog Data Successfully')
