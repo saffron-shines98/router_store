29fa71991c17363568e425c2ee20ce1c1ca4db80
mansigoyal1 <mansi.goyal@plotch.ai>
2024-02-02 23:23:19 +0530

    provider create

--- app/retail/v1/vendor/service.py
+++ app/retail/v1/vendor/service.py
@@ -24,19 +24,6 @@ class VendorService:
         except:
             return self.coordinator.save_data_in_db(log_params, 'plotch_noderetailapi_request_logs')
 
-    def generate_error_log(self, type=None, error_msg=None):
-        log_params = {
-            'request': json.dumps(self.params),
-            'headers': json.dumps(self.headers),
-            'created_at': get_current_datetime(),
-            'type': type,
-            'error_msg': error_msg
-        }
-        try:
-            self.coordinator.save_data_in_db(log_params, 'noderetail_api_request_error_log')
-        except:
-            self.coordinator.save_data_in_db(log_params, 'noderetail_api_request_error_log')
-
     def authenticate_user(self):
         jwt_token = self.headers.get('Auth-Token')
         nodesso_id = self.headers.get('Nodesso-Id')
@@ -49,23 +36,10 @@ class VendorService:
         self.coordinator.validate_jwt(payload)
 
     def create_vendor(self):
+        noderetail_storefront_id = self.params.get('noderetail_storefront_id')
         for provider_details in self.params.get('providers'):
             provider_id = provider_details.get('provider_id')
-            user_instance_id = provider_details.get('noderetail_user_instance_id')
-            log_common_object = [{'col': 'identifier_id', 'val': provider_id},{'col': 'identifier_instance_id', 'val': user_instance_id}]
-            try:
-                log_exist = self.coordinator.get_single_data_from_db('plotch_noderetailapi_request_logs',log_common_object , ['entity_id']).get('entity_id')
-            except:
-                log_exist = self.coordinator.get_single_data_from_db('plotch_noderetailapi_request_logs', log_common_object, ['entity_id']).get('entity_id')
-            if log_exist:
-                raise AlreadyExists('Provider already exists')
-            try:
-                authenticate_user_from_through_sso = authenticate_user(self.headers.get('Auth-Token'), self.headers.get('Nodesso-Id'))
-            except Exception as e:
-                self.generate_error_log(type='vendor', error_msg='Validation Error')
-                raise AuthMissing('Validation Error')
-
-            log_id = self.generate_api_logs(type='vendor', identifier_id=provider_id, identifier_instance_id=user_instance_id)
+            log_id = self.generate_api_logs(type='vendor', identifier_id=provider_id, identifier_instance_id=noderetail_storefront_id)
             try:
                 self.coordinator.push_data_in_queue({'entity_id': log_id}, 'noderetail_provider_create_q')
             except:
