6375ef9f37f3a09605b235722a4b434ce5908da3
tarunchoudhary594 <tarun.choudhary7229@gmail.com>
2024-02-20 17:30:04 +0530

    changes

--- app/retail/v1/inventory/service.py
+++ app/retail/v1/inventory/service.py
@@ -23,9 +23,9 @@ class InventoryService:
             'identifier_instance_id': identifier_instance_id
         }
         try:
-            return self.coordinator.save_data_in_db(log_params, 'plotch_noderetailapi_request_logs')
+            return self.coordinator.save_data_in_db_pool(log_params, 'plotch_noderetailapi_request_logs')
         except:
-            return self.coordinator.save_data_in_db(log_params, 'plotch_noderetailapi_request_logs')
+            return self.coordinator.save_data_in_db_pool(log_params, 'plotch_noderetailapi_request_logs')
 
     def authenticate_user(self):
         jwt_token = self.headers.get('Auth-Token')
