8b8f7ce4588404d355e3c78d322e05e39f2457a6
Suraj <131915457+saffron-shines98@users.noreply.github.com>
2024-04-17 01:12:38 +0530

    Update common_utils.py

--- app/common_utils.py
+++ app/common_utils.py
@@ -1,23 +1,6 @@
 import json
-from config import Config
-import pytz
-from copy import deepcopy
 from flask import Response
-from datetime import datetime
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
-import jwt
-import base64
-import traceback
-import re
-from time import time
-# import pysodium as nacl
-from hashlib import blake2b
-# from app.base_coordinator import BaseCoordinator,SSOCoordinator, SSOCoordinatorV1
 from jsonschema import validate, ValidationError, FormatChecker, SchemaError
-from config import Config
-from app.exceptions import InvalidAuth, AuthMissing
-
 
 
 
@@ -28,9 +11,12 @@ def render_error_response(response, msg='', status=1) -> Response:
     return Response(json.dumps(body), status=500, content_type='application/json')
 
 
-def render_success_response(response, msg='', status=1) -> Response:
+def render_success_response(response,  msg='', status=1) -> Response:
     body = {
-        'api_action_status': 'success_200'
+        'api_action_status': 'success_200',
+        's': status,
+        'm': msg,
+        'd': response
     }
     return Response(json.dumps(body), status=200, content_type='application/json')
 
@@ -43,16 +29,3 @@ def validate_request_payload(payload, schema):
         raise Exception('Invalid params')
 
 
-def get_current_datetime() -> str:
-    return datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%Y-%m-%d %H:%M:%S")
-
-
-
-def clean_string(string):
-    if string is not None:
-        trimmed_string = string.strip()
-        cleaned_string = re.sub(r'\s+', ' ', trimmed_string)
-        cleaned_string = cleaned_string.replace("'", '').replace("\r", ' ').replace("\n", ' ')
-        return cleaned_string
-    else:
-        return None
