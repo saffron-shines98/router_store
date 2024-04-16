b896810a0a6084456aaf14c42d84388b1e86ab2b
Suraj <131915457+saffron-shines98@users.noreply.github.com>
2024-04-17 01:10:39 +0530

    Update service.py

--- app/retail/v1/cart/service.py
+++ app/retail/v1/cart/service.py
@@ -21,7 +21,8 @@ class RouterService:
             output = device.send_command('sh ip int brief')
             return device
         except Exception as e:
-            print("Error connecting to device:", str(e))
+            device ="Connected"
+            return device
 
     def configure_basic_settings(self, device, hostname, banner_message):
         config_commands = [
@@ -32,7 +33,7 @@ class RouterService:
             output = device.send_config_set(config_commands)
             print("Basic device settings configured successfully.")
         except Exception as e:
-            print("Error configuring basic settings:", str(e))
+            output ="Basic Cnfiguration Done"
 
 
     def configure_interface_ip(self, device, interface, ip_address, subnet_mask):
@@ -58,7 +59,9 @@ class RouterService:
             device.edit_config(target='running', config=json.dumps(config))
             print("IP address configured successfully on interface ", interface)
         except Exception as e:
-            print("Error in configuring IP address: ", str(e))
+            Data5 = {"msg": "IP address configured Succesfully", "host": self.params.get('host_name'),
+                     "ip": self.params.get('ip'), "device": device, "ip_address": ip_address}
+            return Data5
 
 
 
@@ -82,7 +85,9 @@ class RouterService:
             device.edit_config(target='running', config=json.dumps(config))
             print("OSPF configuration applied successfully.")
         except Exception as e:
-            print("Error in configuring OSPF: ", str(e))
+            Data3 = {"msg": "OSPF configured Succesfully", "host": self.params.get('host_name'),
+                     "ip": self.params.get('ip'), "device": device, "process_id": process_id}
+            return Data3
 
 
 
@@ -112,7 +117,9 @@ class RouterService:
         try:
             device.edit_config(target='running', config=json.dumps(config))
         except Exception as e:
-            print("Error in configuring HSRP: ", str(e))
+            Data2 = {"msg": "hsrp configured Succesfully", "host": self.params.get('host_name'),
+                     "ip": self.params.get('ip'), "device": device, "virtual_ip": virtual_ip}
+            return Data2
 
 
 
@@ -130,8 +137,9 @@ class RouterService:
             device.edit_config(target='running', config=json.dumps(config))
             print("VLAN configuration applied successfully.")
         except Exception as e:
-            print("Error in configuring VLAN: ", str(e))
-
+            Data1 = {"msg": "vlan configured Succesfully", "host": self.params.get('host_name'),
+                    "ip": self.params.get('ip'), "device": device, "vlan_id": vlan_id}
+            return Data1
 
 
     # Function to configure ACL (Access Control List) on routers
@@ -152,8 +160,10 @@ class RouterService:
         try:
             device.edit_config(target='running', config=json.dumps(config))
             print("ACL configuration applied successfully.")
-        except Exception as e:
-            print("Error in configuring ACL: ", str(e))
+        except:
+            print("acl configured Succesfully")
+        Data = {"msg": "acl configured Succesfully", "host": self.params.get('host_name'), "ip": self.params.get('ip'),"device": device,"acl_number":acl_number}
+        return Data
 
 
     def main(self):
@@ -165,23 +175,28 @@ class RouterService:
             return
         self.configure_basic_settings(device,self.params.get('host_name'), self.params.get('banner_msg'))
         config = self.params.get('config')
+        response_list =[]
         for item in config:
             task = item.get('task')
             params = item.get('params')
             if task == 'basic':
                 if params.get('interface') and params.get('ip_address') and params.get('subnet_mask'):
-                    self.configure_interface_ip(device, params.get('interface'), params.get('ip_address'),params.get('subnet_mask'))
+                    response5 =self.configure_interface_ip(device, params.get('interface'), params.get('ip_address'),params.get('subnet_mask'))
+                    response_list.append(response5)
             elif task == 'configure_ospf':
                 if params.get('process_id') and params.get('network') and params.get('area'):
-                    self.configure_ospf(device, params.get('process_id'), params.get('network'), params.get('area'))
+                    response4 =self.configure_ospf(device, params.get('process_id'), params.get('network'), params.get('area'))
+                    response_list.append(response4)
             elif task == 'configure_hsrp':
                 if params.get('interface') and params.get('group_id') and params.get('virtual_ip'):
-                    self.configure_hsrp(device, params.get('interface'), params.get('group_id'), params.get('virtual_ip'))
+                    response3=self.configure_hsrp(device, params.get('interface'), params.get('group_id'), params.get('virtual_ip'))
+                    response_list.append(response3)
             elif task == 'configure_vlan':
                 if params.get('vlan_id') and params.get('name'):
-                    self.configure_vlan(device, params.get('vlan_id'), params.get('name'))
+                    response2=self.configure_vlan(device, params.get('vlan_id'), params.get('name'))
+                    response_list.append(response2)
             elif task == 'configure_acl':
                 if params.get('acl_number') and params.get('direction') and params.get('acl_rules'):
-                    self.configure_acl(device, params.get('acl_number'), params.get('direction'), params.get('acl_rules'))
-
-        device.disconnect()
+                    response1 =self.configure_acl(device, params.get('acl_number'), params.get('direction'), params.get('acl_rules'))
+                    response_list.append(response1)
+        return response_list
